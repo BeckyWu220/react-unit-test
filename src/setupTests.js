577c5b7abf0e665c35f2bd530733d8c017c67310
Becky Wu <becky.wu@twotalltotems.com>
2019-11-24 14:25:47 -0800

    Place Enzyme configuration into separate ffile to run automatically in each test file

---
+++ src/setupTests.js
@@ -0,0 +1,6 @@
+//This script will be run before every test file.
+
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
