ddb2a2ad582ba2e7a40335cc226a43a5aa748752
Becky Wu <wwq220@gmail.com>
2020-01-12 18:14:30 -0800

    Created Input test to display input and submit button properly according to  state from redux store.

--- test/testUtils.js
+++ test/testUtils.js
@@ -12,4 +12,6 @@ import { createStore } from 'redux';
 import rootReducer from '../src/reducers';
 export const storeFactory = (initialState) => {
     return createStore(rootReducer, initialState);
+    //Here we create a actual store instead of mocking one.
+    //redux-mock-store can be used to mock store. However, it couldn't test changes to state.
 }
